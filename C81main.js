3637b89139c7ae5ff30d9018b1d4abb1985de2e1
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-07-28 09:26:48 +0530

    v1.0
    Added 
    - Circle Drawing
    - Set Own Color

---
+++ C81main.js
@@ -0,0 +1,24 @@
+canvas=document.getElementById("my_Canvas");
+ctx=canvas.getContext("2d");
+color="black";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",myMouseCanvas);
+function myMouseCanvas(e)
+{
+mouse_x=e.clientX-canvas.offsetLeft;
+mouse_y=e.clientY-canvas.offsetTop;
+console.log("x= "+mouse_x+"y= "+mouse_y);
+circle(mouse_x,mouse_y);
+}
+function circle(mouse_x,mouse_y)
+{
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
+ctx.stroke();
+}
