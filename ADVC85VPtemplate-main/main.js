909835775f417af95e345b268c26b5ae1a6f6f75
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-08-12 16:52:11 +0530

    v1.5.1
    easter evolution

---
+++ ADVC85VPtemplate-main/main.js
@@ -0,0 +1,83 @@
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+greencar_width=75;
+greencar_height= 100;
+greencar_x=5;
+greencar_y=225;
+
+function add() {
+	background_imgTag=newImage();
+	background_imgTag.onload=uploadBackground;
+	background_imgTag.src=background_image;
+	greencar_imgTag=newImage();
+	greencar_imgTag.onload=uploadgreencar;
+	greencar_imgTag.src=greencar_image;
+}
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height)
+
+}
+
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	//Define function to move the car upward
+}
+
+function down()
+{
+	//Define function to move the car downward
+}
+
+function left()
+{
+	//Define function to move the car left side
+}
+
+function right()
+{
+	//Define function to move the car right side
+}
